68ed306464bd92151165a3046b14f7da930284ca
venya <ve.po2014@yandex.ru>
2024-07-15 12:38:22 +0700

    2 views for mark and model were implemented

--- Django/bibinet/search_parts/management/commands/populate_initial_data.py
+++ Django/bibinet/search_parts/management/commands/populate_initial_data.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from search_parts.models import Mark, Model
 
 
 class Command(BaseCommand):
@@ -19,3 +20,17 @@ class Command(BaseCommand):
             {"name": "Mustang", "mark_name": "Ford", "is_visible": True},
             {"name": "Golf", "mark_name": "Volkswagen", "is_visible": True}
         ]
+
+        for mark_data in marks:
+            mark, created = Mark.objects.get_or_create(name=mark_data["name"],
+                                                       defaults=mark_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Mark {mark.name} created.'))
+
+        for model_data in models:
+            mark = Mark.objects.get(name=model_data["mark_name"])
+            model, created = Model.objects.get_or_create(name=model_data["name"],
+                                                         mark=mark,
+                                                         defaults={'is_visible': model_data['is_visible']})
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Model {model.name} created.'))
--- Django/bibinet/search_parts/views.py
+++ Django/bibinet/search_parts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Mark, Model, Part
 
-# Create your views here.
+
+def mark_list(request):
+    marks = list(Mark.objects.values())
+    return JsonResponse(marks, safe=False)
+
+
+def model_list(request):
+    models = list(Model.objects.values())
+    return JsonResponse(models, safe=False)
