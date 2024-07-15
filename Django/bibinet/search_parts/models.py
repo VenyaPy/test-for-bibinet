e4ae112007b2f6923d369d65add046c64777b7e9
venya <ve.po2014@yandex.ru>
2024-07-15 12:05:18 +0700

    Models for search_parts have been added

--- Django/bibinet/search_parts/models.py
+++ Django/bibinet/search_parts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Mark(models.Model):
+    name = models.CharField(max_length=255)
+    producer_country_name = models.CharField(max_length=255)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['producer_country_name']),
+        ]
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=255)
+    mark = models.ForeignKey(Mark, on_delete=models.CASCADE)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+
+class Part(models.Model):
+    name = models.CharField(max_length=255)
+    mark = models.ForeignKey(Mark, on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    json_data = models.JSONField(blank=True, null=True)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['json_data']),
+        ]
+
